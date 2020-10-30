e879794d542f1f1e4a7c3f36acedd8ef0ece0ecf
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-10-30 21:00:48 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -1,33 +1,67 @@
-import React, { Component } from 'react';
-import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js'
+import React from 'react';
+import ToyHeader from './ToyHeader.js'
+import ToyForm from './ToyForm.js'
+import ToysContainer from './ToysContainer.js'
 
-
-export default class ChromeBoisDomain extends Component {
-  
-  handleMouseMove = (event) => {
-    let x = event.clientX
-    let y = event.clientY
-     drawChromeBoiAtCoords(x, y)
-  
-  
- 
-    let size = event.key
-
-    if (size === 'a') {
-      resize('+')
-    } else if (size === 's') {
-      resize('-')
-    }
+export default class App extends React.Component {
+  state = {
+    toysArray: []
   }
-  
+
+  componentDidMount() {
+   fetch('http://localhost:3000/toys')
+   .then(res => res.json())
+   .then(severData => this.setState({toysArray: severData}))
+  }
+
+  addLike = (e) => {
+    // need a conditinal data-toyname == obj.name do this 
+    // else return obj
+    let toyName = e.target.dataset.toyname;
+
+    this.setState((preState) => {
+      return (
+        { 
+          toysArray: preState.toysArray.map( toyObj => {
+            if(toyObj.name === toyName){
+              return {...toyObj, likes: toyObj.likes + 1 }
+            } else {
+              return toyObj
+            }
+          })
+        }
+      )
+    }, () => console.log(this.state))
+    // console.log(e.target)
+  }
+
+  addNewToy = (newToy) => {
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(res => res.json())
+    .then(toy => this.setState(
+      (preState) => {
+        return {toysArray: [...preState.toysArray, toy]}
+      }, 
+      () => console.log(this.state.toysArray)
+    ))
+  }
+
   render() {
     return (
-      <canvas 
-        onMouseMove={this.handleMouseMove}
-        width='900'
-        height='600'
-        tabIndex="0">
-      </canvas>
-    )
-  }
+      <div className="Main">
+        <ToyHeader />
+        <ToyForm addToyToToyArr={this.addNewToy} />
+        <ToysContainer 
+          toysArray={this.state.toysArray} 
+          addLikeFn={this.addLike}
+        />
+      </div>
+    )}
 }
